f068192d437aef735aff1525d26d9d396f03e13d
Maximilian König <makoeta@gmx.com>
2024-06-26 11:07:02 +0200

    final game

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# exitgame
+# exitgame: technische Dokumentation
+
+> Wokwi-Projekt: [Link](https://wokwi.com/projects/401155421833016321)
+
+## Aufbau / Rätsel
+
+![Aufbau_Rätseö.png](Aufbau_Rätsel.png)
+
+> **Hinweis**: Kabel an Display lassen.
+
+## Aufbau / Lösung
+
+![Aufbau.png](Aufbau.png)
+
+> **Hinweis**: Die türkise Lampe (rechts) soll die Schwarzlicht-LED darstellen.
+
+Alternativ kann die Diagram.json auch in ein [Wokwi.com](https://wokwi.com)-Projekt gekopiert werden.
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,15 @@
 #define LCD_LINES 2
 
 #define RED_LED 11
-#define ORANGE_LED 10
-#define YELLOW_LED 9
-#define GREEN_LED 6
-#define BLUE_LED 5
+#define YELLOW_LED 10
+#define GREEN_LED 9
+#define BLUE_LED 6
 
 // LED CODE
-#define RED_CODE 4
-#define ORANGE_CODE 3
-#define YELLOW_CODE 2
+#define RED_CODE 2
+#define YELLOW_CODE 3
 #define GREEN_CODE 1
+#define BLUE_CODE 2
 
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
@@ -36,13 +35,6 @@ void playLedCode()
 
   delay(400);
 
-  for (int i = 0; i < ORANGE_CODE; i++)
-  {
-    blinkLed(ORANGE_LED);
-  }
-
-  delay(400);
-
   for (int i = 0; i < YELLOW_CODE; i++)
   {
     blinkLed(YELLOW_LED);
@@ -57,9 +49,11 @@ void playLedCode()
 
   delay(400);
 
-  analogWrite(BLUE_LED, HIGH);
-  delay(2000);
-  analogWrite(BLUE_LED, LOW);
+  for (int i = 0; i < BLUE_CODE; i++)
+  {
+    blinkLed(BLUE_LED);
+  }
+  
   delay(2000);
 }
 
@@ -69,8 +63,8 @@ void setup()
   lcd.init();
   lcd.backlight();
 
-  lcd.setCursor(3, 0);
-  lcd.print("LOESUNG HIER");
+  lcd.setCursor(1, 0);
+  lcd.print("Hacker Attack");
 
   playLedCode();
 }
